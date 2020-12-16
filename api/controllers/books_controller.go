8ad63bd3ef72ed6e12364accda84425034a37b41
filippoipp <filippoipp@hotmail.com>
2020-12-16 00:07:41 -0300

    feat: veirfy if book exist and just the booker owner can land it

--- api/controllers/books_controller.go
+++ api/controllers/books_controller.go
@@ -45,6 +45,39 @@ func (server *Server) CreateBook(w http.ResponseWriter, r *http.Request) {
 
 func (server *Server) LendBook(w http.ResponseWriter, r *http.Request) {
 
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+
+	t := struct {
+		LoggedUserID *uint64 `json:"logged_user_id"`
+		BookID *int `json:"book_id"`
+		ToUserID *int `json:"to_user_id"`// pointer so we can test for field absence
+	}{}
+
+	err := d.Decode(&t)
+	if err != nil {
+		// bad JSON or unrecognized json field
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(*t.LoggedUserID)
+
+
+	// Check if the book exist
+	book := models.Book{}
+	err = server.DB.Debug().Model(models.Book{}).Where("id = ?", *t.BookID).Take(&book).Error
+	if err != nil {
+		responses.ERROR(w, http.StatusNotFound, errors.New("Book not found"))
+		return
+	}
+
+	// If a user attempt to lend a book not belonging to him
+	if *t.LoggedUserID != book.LoggedUserID {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
 	// Read the data
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -54,25 +87,9 @@ func (server *Server) LendBook(w http.ResponseWriter, r *http.Request) {
 
 	// Start processing the request data
 	bookUpdate := models.Book{}
-	err = json.Unmarshal(body, &bookUpdate)
+	err = json.Unmarshal(body, &t)
 	if err != nil {
 		responses.ERROR(w, http.StatusUnprocessableEntity, err)
-		return
-	}
-
-	fmt.Print(bookUpdate)
-
-	// Check if the book exist
-	book := models.Book{}
-	err = server.DB.Debug().Model(models.Book{}).Where("id = ?", body[1]).Take(&book).Error
-	if err != nil {
-		responses.ERROR(w, http.StatusNotFound, errors.New("Post not found"))
-		return
-	}
-
-	// If a user attempt to lend a book not belonging to him
-	if 2 != book.LoggedUserID {
-		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
 		return
 	}
 
